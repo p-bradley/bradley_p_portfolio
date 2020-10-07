5feb339ec4e01a379a01a8647a37b435ec1e3feb
Austin Caron <austin.caron1@gmail.com>
2020-10-07 16:18:19 -0400

    adding db fixes

--- config/config.php
+++ config/config.php
@@ -1,16 +1,16 @@
 <?php 
 
     $db_dsn = array(
-        'host' => 'localhost',
-        'dbname' => 'db_bradley',
+        'host' => 'us-cdbr-east-02.cleardb.com',
+        'dbname' => 'heroku_8574740f7e7733e',
         'charset' => 'utf8',
     );
     
     $dsn = 'mysql:' . http_build_query($db_dsn, '', ';');
 
     // Set up connection credentials
-    $db_user = 'root';
-    $db_pass = '';
+    $db_user = 'b4894a68fed187';
+    $db_pass = '260c7145';
 
     $pdo = new PDO($dsn, $db_user, $db_pass);
 
